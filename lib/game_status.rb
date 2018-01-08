d435206b2ad77ec70df6d5d66100a4683a5d2cf7
Dalton Fant <daltonfant16@gmail.com>
2018-01-08 23:36:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,8 +48,7 @@ def winner(board)
       return "X"
     elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
       return "O"
-    else
-      return nil
     end
   end
+  return nil 
 end
